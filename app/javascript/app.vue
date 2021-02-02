ca7a31aae51dea13c58e9ce505536e6526642c34
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-03 06:59:29 +0900

    Fix

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <v-app id="app">
     <v-form
       :model="toDo"
       :action="toDo.action"
@@ -30,7 +30,7 @@
         native-type="submit">作成
       </v-btn>
     </v-form>
-  </div>
+  </v-app>
 </template>
 
 <script>
