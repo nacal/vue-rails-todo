2e527a74699f874abfd54aa9b9fa14fc0ebbf43d
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-03 05:57:06 +0900

    Develop checkbox ToDo

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -3,11 +3,18 @@
     <v-data-table
       :headers="headers"
       :items="toDos"
+      class="table"
     >
-      <template v-slot:[`item.action`]="{ item }">
-        <v-btn @click="destroyToDo(item.id)">
+      <template v-slot:[`item.delete`]="{ item }">
+        <v-btn @click="destroyToDo(item.id)" block>
           <v-icon dense>mdi-delete</v-icon>
         </v-btn>
+      </template>
+      <template v-slot:[`item.finished`]="{ item }">
+        <v-checkbox
+          v-model="item.finished"
+          @change="updateToDo(item.id, item.finished)"
+        ></v-checkbox>
       </template>
     </v-data-table>
   </div>
@@ -22,6 +29,12 @@ import {reject} from 'lodash'
       return {
         headers: [
           {
+            text: 'finished',
+            value: 'finished',
+            sortable: false,
+            width: '120px'
+          },
+          {
             text: 'title',
             value: 'title'
           },
@@ -31,11 +44,11 @@ import {reject} from 'lodash'
             width: '240px'
           },
           {
-            text: 'Action',
-            value: 'action',
+            text: 'delete',
+            value: 'delete',
             sortable: false,
             width: '120px'
-          },
+          }
         ],
         toDos: []
       }
@@ -54,7 +67,21 @@ import {reject} from 'lodash'
               this.toDos = reject(this.toDos, ['id', id])
             }
           })
+      },
+      updateToDo(id, finished) {
+        axios.patch('/api/v1/to_dos/' + id, {to_do: {finished: finished}})
+          .then(res => {
+            if (res.status === 200) {
+              console.log(res)
+            }
+        })
       }
     }
   }
 </script>
+
+<style scoped>
+.table{
+  text-align: left;
+}
+</style>>
