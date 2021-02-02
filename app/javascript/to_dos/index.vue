0cd6981ac8d81e4a32272ff5a9cbab0277874c5b
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-02-03 05:28:29 +0900

    Develop delete ToDo

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="toDos"
-  ></v-data-table>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="toDos"
+    >
+      <template v-slot:[`item.action`]="{ item }">
+        <v-btn @click="destroyToDo(item.id)">
+          <v-icon dense>mdi-delete</v-icon>
+        </v-btn>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import {reject} from 'lodash'
 
   export default {
     data() {
@@ -19,16 +28,33 @@ import axios from 'axios'
           {
             text: 'expired_at',
             value: 'expired_at',
-          }
+            width: '240px'
+          },
+          {
+            text: 'Action',
+            value: 'action',
+            sortable: false,
+            width: '120px'
+          },
         ],
         toDos: []
       }
     },
     created() {
-    axios.get('/api/v1/to_dos')
-       .then(res => {
-         this.toDos = res.data
-       })
-  }
+      axios.get('/api/v1/to_dos')
+        .then(res => {
+          this.toDos = res.data
+        })
+    },
+    methods: {
+      destroyToDo(id) {
+        axios.delete('/api/v1/to_dos/' + id)
+          .then(res => {
+            if (res.status === 200) {
+              this.toDos = reject(this.toDos, ['id', id])
+            }
+          })
+      }
+    }
   }
 </script>
